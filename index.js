5faad3bcd236295738eb1ec08f36ac54d1587c5d
yash ghantala <yashghantala@gmail.com>
2022-03-17 19:07:42 +0530

    ghost repo initialized

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.use('/public', express.static(path.join(__dirname + '/public')))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Discussion Server Started on Port ${port}`)
+})
